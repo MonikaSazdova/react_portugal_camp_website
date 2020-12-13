1442db9beb4ab5fcc74f2652450c87342eec2270
Monika Sazdova <59760777+MonikaSazdova@users.noreply.github.com>
2020-12-13 19:33:43 +0100

    images update

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,15 +9,15 @@ function Cards() {
 			<h1>Check out these EPIC Portuguese destinations!</h1>
 			<div className="cards__container">
 				<div className="cards__wrapper">
-				<ul className="cards__items">
-						<CardItem 
-							src={process.env.PUBLIC_URL + 'images/img-9.jpg'}
-							text="Explore the hidden waterfall deep inside Madeira Island"
+					<ul className="cards__items">
+					<CardItem 
+							src={process.env.PUBLIC_URL + 'images/img-14.jpg'}
+							text="Explore the beauty of the Atlantic Ocean"
 							label='Adventure'
 							path="/services"
 						/>
 						<CardItem 
-							src={process.env.PUBLIC_URL + 'images/img-2.jpg'}
+							src={process.env.PUBLIC_URL + 'images/img-12.jpg'}
 							text="Travel through the Azores Islands"
 							label='Explore'
 							path="/services"
@@ -25,23 +25,24 @@ function Cards() {
 					</ul>
 					<ul className="cards__items">
 						<CardItem 
-							src={process.env.PUBLIC_URL + 'images/img-9.jpg'}
+							src={process.env.PUBLIC_URL + 'images/img-11.jpg'}
+							text="Enjoy in the beauty of Algarve"
+							label='Adventure'
+							path="/services"
+						/>
+						<CardItem 
+							src={process.env.PUBLIC_URL + 'images/img-10.jpg'}
+							text="Explore the amazing beaches of Portugal"
+							label='Explore'
+							path="/services"
+						/>
+									<CardItem 
+							src={process.env.PUBLIC_URL + 'images/img-13.jpg'}
 							text="Explore the hidden waterfall deep inside Madeira Island"
 							label='Adventure'
 							path="/services"
 						/>
-						<CardItem 
-							src={process.env.PUBLIC_URL + 'images/img-2.jpg'}
-							text="Travel through the Azores Islands"
-							label='Explore'
-							path="/services"
-						/>
-						<CardItem 
-							src={process.env.PUBLIC_URL + 'images/img-9.jpg'}
-							text=""
-							label='Adventure'
-							path="/services"
-						/>
+				
 					</ul>
 				</div>
 			</div>
